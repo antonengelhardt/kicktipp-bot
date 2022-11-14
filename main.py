8367b874a26440fd77520ef63695fa5f7df58c5d
Anton Engelhardt <anton.engelhardt@savestrike.de>
2022-11-14 22:51:38 +0100

    adjusted for dockerfile changes, changed tip calc

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
 
 # Constants
 
@@ -16,13 +17,16 @@ PASSWORD = os.getenv("KICKTIPP_PASSWORD")
 NAME_OF_COMPETITION = os.getenv("KICKTIPP_NAME_OF_COMPETITION")
 CHROMEDRIVER_PATH = "/Applications/chromedriver"
 # DAY_OF_EXECUTION = os.getenv("DAY_OF_EXECUTION")  # wednesday
-DAY_OF_EXECUTION = 0
+DAY_OF_EXECUTION = 1
 
 
 def execute():
 
     # create driver
-    driver = webdriver.Chrome(CHROMEDRIVER_PATH)
+    # driver = webdriver.Chrome(options=set_chrome_options()) # for docker
+    driver = webdriver.Chrome(CHROMEDRIVER_PATH)  # for local
+
+    # login
     driver.get(LOGIN_URL)
 
     # enter email
@@ -37,63 +41,51 @@ def execute():
     # accept AGB
     try:
         driver.find_element(
-        by=By.XPATH, value='//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]').click()
+            by=By.XPATH, value='//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]').click()
     except NoSuchElementException:
         pass
-    
-    # select competition
-    driver.find_element(
-        by=By.PARTIAL_LINK_TEXT, value=str(NAME_OF_COMPETITION)).click()
 
-    # select entry item
-    driver.find_element(
-        by=By.XPATH, value='//*[@id="navigation"]/div[5]/a').click()
-
-    sleep(0.1)
+    # entry form
+    driver.get(F"https://www.kicktipp.de/{NAME_OF_COMPETITION}/tippabgabe")
 
     # iterate over rows of the form
     for i in range(1, 10):
-        sleep(0.1)
-        # find quotes
-        quotes = driver.find_element(
-            by=By.XPATH, value='//*[@id="tippabgabeSpiele"]/tbody/tr[' + str(i) + ']/td[5]/a')
-        content = quotes.get_property('innerHTML')
-        # split quotes by seperator
-        splitted = str.split(content, sep=' / ')
-
-        # get Team names
-        homeTeam = driver.find_element(
-            by=By.XPATH, value='//*[@id="tippabgabeSpiele"]/tbody/tr[' + str(i) + ']/td[2]').get_attribute('innerHTML')
-        awayTeam = driver.find_element(
-            by=By.XPATH, value='//*[@id="tippabgabeSpiele"]/tbody/tr[' + str(i) + ']/td[3]').get_attribute('innerHTML')
-
-        # print quotes and team names
-        print(homeTeam + " - " + awayTeam + "\nQuotes:" + str(splitted))
-
-        # calculate tips bases on quotes and print them
-        tip = calculate_tip(float(splitted[0]), float(
-            splitted[1]), float(splitted[2]))
-        print("Tip:" + str(tip))
-        print()
-
         try:
-        # find entry, clear it and enter tip
+            # find quotes
+            quotes = driver.find_element(
+                by=By.XPATH, value='//*[@id="tippabgabeSpiele"]/tbody/tr[' + str(i) + ']/td[5]/a')
+            content = quotes.get_property('innerHTML')
+            # split quotes by seperator
+            splitted = str.split(content, sep=' / ')
+
+            # get Team names
+            homeTeam = driver.find_element(
+                by=By.XPATH, value='//*[@id="tippabgabeSpiele"]/tbody/tr[' + str(i) + ']/td[2]').get_attribute('innerHTML')
+            awayTeam = driver.find_element(
+                by=By.XPATH, value='//*[@id="tippabgabeSpiele"]/tbody/tr[' + str(i) + ']/td[3]').get_attribute('innerHTML')
+
+            # print quotes and team names
+            print(homeTeam + " - " + awayTeam + "\nQuotes:" + str(splitted))
+
+            # calculate tips bases on quotes and print them
+            tip = calculate_tip(float(splitted[0]), float(
+                splitted[1]), float(splitted[2]))
+            print("Tip:" + str(tip))
+            print()
+
+            # find entry, clear it and enter tip
             homeTipEntry = driver.find_element(by=By.XPATH,
-                                           value='//*[@id="tippabgabeSpiele"]/tbody/tr[' + str(i) + ']/td[4]/input[2]')
+                                               value='//*[@id="tippabgabeSpiele"]/tbody/tr[' + str(i) + ']/td[4]/input[2]')
             homeTipEntry.clear()
             homeTipEntry.send_keys(tip[0])
-        except NoSuchElementException:
-            pass
-
-
-        try:
-        # find entry, clear it and enter tip
+            
+            # find entry, clear it and enter tip
             awayTipEntry = driver.find_element(by=By.XPATH,
-                                           value='//*[@id="tippabgabeSpiele"]/tbody/tr[' + str(i) + ']/td[4]/input[3]')
+                                               value='//*[@id="tippabgabeSpiele"]/tbody/tr[' + str(i) + ']/td[4]/input[3]')
             awayTipEntry.clear()
             awayTipEntry.send_keys(tip[1])
         except NoSuchElementException:
-            pass
+            continue
     # submit all tips
     driver.find_element(by=By.NAME, value="submitbutton").click()
 
@@ -111,7 +103,7 @@ def calculate_tip(home, draw, away) -> (int, int):
     onemore = round(random.uniform(0, 1))
 
     # depending on the quotes, the factor is derived to decrease the tip for very unequal games
-    coefficient = 0.3 if differenceHomeAndAway > 8 else 0.9
+    coefficient = 0.3 if abs(differenceHomeAndAway) > 8 else 0.8
 
     # calculate tips
     if abs(differenceHomeAndAway) < 0.25:
@@ -125,11 +117,25 @@ def calculate_tip(home, draw, away) -> (int, int):
             return onemore, onemore
 
 
+def set_chrome_options() -> None:
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
+
+
 if __name__ == '__main__':
     while True:
         now = datetime.now()
         formatted = now.strftime("%w")
-        
+
         if formatted == str(DAY_OF_EXECUTION):
             print("The Script will execute now!")
             execute()
